3212624be35e59d0ffafe61e2a73dca3ba293143
Kilig <3156535770@qq.com>
2023-11-17 22:37:27 +0800

    添加注释

--- giegie.c
+++ giegie.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-int main()
+#include<stdio.h>//包含了标准输入输出库 
+int main()//主函数 
 {
-	printf("hello world!");
-	return 0;
+	printf("hello world!");//打印出"hello world!" 
+	return 0;//返回0，程序正常结束 
 }
