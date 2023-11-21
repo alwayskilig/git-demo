c9143a420cb1e28e4a9a41c3d509feb92d943dd1
Kilig <3156535770@qq.com>
2023-11-21 16:36:20 +0800

    dev first commit

--- giegie.c
+++ giegie.c
@@ -1,6 +1,7 @@
-#include<stdio.h>//包含了标准输入输出库 
-int main()//主函数 
-{
-	printf("hello world!");//打印出"hello world!" 
+#include<stdio.h>
+
+int main(){//主函数 {
+	printf("hello world!\n");//打印出"hello world!" 
+	printf("I love rococo");
 	return 0;//返回0，程序正常结束 
 }
